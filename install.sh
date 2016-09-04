86a41318d6f48656affe6a9d0a20bb56abf4d793
Jonas Weigert <weigert.jonas@gmail.com>
2016-09-03 20:32:26 -0500

    add sqlite and mcrypt dependencies for php7.0

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ sudo apt-get update
 # Install Stack
 sudo apt-get -y install -qq php7.0 php7.0-fpm php7.0-cli php7.0-common php7.0-json php7.0-opcache php7.0-mysql php7.0-phpdbg \
 php7.0-mbstring php7.0-gd php7.0-imap php7.0-ldap php7.0-pgsql php7.0-pspell php7.0-recode php7.0-tidy php7.0-dev \
-php7.0-intl php7.0-gd php7.0-curl php7.0-zip php7.0-xml redis-server mysql-server beanstalkd postgresql postgresql-contrib
+php7.0-intl php7.0-gd php7.0-curl php7.0-zip php7.0-xml php7.0-mcrypt php7.0-sqlite redis-server mysql-server beanstalkd postgresql postgresql-contrib
 
 sudo apt-get purge -qq apache2 mysql-server mysql-client
 
