efc2c9afcf2d49916e45afdc9c3de7249ccd1701
Jonas Weigert <weigert.jonas@gmail.com>
2017-12-10 11:50:40 -0600

    Revert "upgrade php 7.0 branch to postgres to 9.6"
    This reverts commit ca4b56cab9682d55d32a1a3bd9ab1a5c80b7c2a0.

--- install.sh
+++ install.sh
@@ -11,10 +11,6 @@ if [ -f /etc/apt/sources.list ] && [ -L /etc/apt/sources.list ]; then
   sudo cp /etc/apt/sources.list.old /etc/apt/sources.list
 fi
 
-# ADD Postgres 9.6
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
-wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | sudo apt-key add -
-
 # Update Composer
 sudo /usr/bin/composer self-update
 
@@ -26,7 +22,7 @@ sudo apt-get update
 # Install Stack
 sudo apt-get -y install -qq php7.0 php7.0-fpm php7.0-cli php7.0-common php7.0-json php7.0-opcache php7.0-mysql php7.0-phpdbg \
 php7.0-mbstring php7.0-gd php7.0-imap php7.0-ldap php7.0-pgsql php7.0-pspell php7.0-recode php7.0-tidy php7.0-dev \
-php7.0-intl php7.0-gd php7.0-curl php7.0-zip php7.0-xml php7.0-mcrypt php7.0-sqlite php7.0-soap redis-server mysql-server beanstalkd postgresql-9.6 postgresql-contrib
+php7.0-intl php7.0-gd php7.0-curl php7.0-zip php7.0-xml php7.0-mcrypt php7.0-sqlite php7.0-soap redis-server mysql-server beanstalkd postgresql postgresql-contrib
 
 sudo apt-get purge -qq apache2 mysql-server mysql-client
 
