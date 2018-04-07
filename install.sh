d360a52fccece905d26a7cd6cd78c22e1453c88c
Jonas Weigert <weigert.jonas@gmail.com>
2018-04-07 11:02:41 -0500

    update script to install mysql 5.7 by default

--- install.sh
+++ install.sh
@@ -14,15 +14,30 @@ fi
 # Update Composer
 sudo /usr/bin/composer self-update
 
-# Add PHP7.0 Repository
+# Add PHP7 Repository
 LC_ALL=C.UTF-8 sudo add-apt-repository ppa:ondrej/php -y
 sudo apt-add-repository ppa:rwky/redis -y
+
+# Add MySQL 5.7
+export DEBIAN_FRONTEND=noninteractive
+debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/repo-codename select trusty'
+debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/repo-distro select ubuntu'
+debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/repo-url string http://repo.mysql.com/apt/'
+debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/select-preview select '
+debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/select-product select Ok'
+debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/select-server select mysql-5.7'
+debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/select-tools select '
+debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/unsupported-platform select abort'
+wget http://dev.mysql.com/get/mysql-apt-config_0.7.2-1_all.deb
+dpkg -i mysql-apt-config_0.7.2-1_all.deb
+
+#APT: Update
 sudo apt-get update
 
 # Install Stack
 sudo apt-get -y install -qq php7.1 php7.1-fpm php7.1-cli php7.1-common php7.1-json php7.1-opcache php7.1-mysql php7.1-phpdbg \
 php7.1-mbstring php7.1-gd php7.1-imap php7.1-ldap php7.1-pgsql php7.1-pspell php7.1-recode php7.1-tidy php7.1-dev \
-php7.1-intl php7.1-gd php7.1-curl php7.1-zip php7.1-xml php7.1-mcrypt php7.1-sqlite php7.1-soap redis-server mysql-server beanstalkd postgresql postgresql-contrib
+php7.1-intl php7.1-gd php7.1-curl php7.1-zip php7.1-xml php7.1-mcrypt php7.1-sqlite php7.1-soap redis-server mysql-server-5.7 beanstalkd postgresql postgresql-contrib
 
 sudo apt-get purge -qq apache2 mysql-server mysql-client
 
